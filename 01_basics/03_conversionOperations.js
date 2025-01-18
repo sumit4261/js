be7bc3a317bf889197c1601776e2fd92a6b0567b
Sumit Mane <153489815+sumit4261@users.noreply.github.com>
2025-01-18 07:08:09 +0000

    Conversion Of Datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,27 @@
+//let score = "33abc";
+
+//console.log(typeof score)
+//console.log(typeof(score))
+
+//let valueInNumber = Number(score);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1; false => 0
+//null => 0
+
+let isloggedin = "Sumit";
+
+let booleanisloggedin = Boolean(isloggedin);
+console.log(booleanisloggedin)
+
+//1 => true; 0 = false;
+//"" => false
+//"sumit" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber);
+console.log(typeof stringNumber)

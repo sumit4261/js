e8ede1d21a05307ed113272ee6e2cd88d13be3dd
Sumit Mane <153489815+sumit4261@users.noreply.github.com>
2025-01-18 07:29:21 +0000

    Operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -15,7 +15,7 @@
 let isloggedin = "Sumit";
 
 let booleanisloggedin = Boolean(isloggedin);
-console.log(booleanisloggedin)
+//console.log(booleanisloggedin)
 
 //1 => true; 0 = false;
 //"" => false
@@ -24,4 +24,25 @@ console.log(booleanisloggedin)
 let someNumber = 33
 
 let stringNumber = String(someNumber);
-console.log(typeof stringNumber)
+//console.log(typeof stringNumber)
+
+
+//operations
+
+let value = 3
+let negValue = -value
+//console.log(negValue)
+
+let str1 = "hello"
+let str2 = " sumit"
+
+let str3 = str1 + str2;
+//console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+
+

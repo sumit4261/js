e968f9bd17d3c20d9264efefd56a88d006ce0ff0
Sumit Mane <153489815+sumit4261@users.noreply.github.com>
2025-01-18 06:45:25 +0000

    dataypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,16 @@
+"use strict"  //treat all JS code as newer version
+
+//alert(3 + 3) //we are using nodejs, not browser
+
+//DATATYPES
+let name = "sumit"
+let age = 20
+
+//number => 2 to power 53
+//bigint 
+//boolean => true/false
+//null => standalone value
+//undefined
+//symbol=> unique
+
+//object

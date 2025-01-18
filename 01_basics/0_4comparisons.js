59dab13c1bfe30286df1137e109a0360c1875811
Sumit Mane <153489815+sumit4261@users.noreply.github.com>
2025-01-18 07:39:43 +0000

    Comparison

---
+++ 01_basics/0_4comparisons.js
@@ -0,0 +1,12 @@
+//comparison btn 2 different dataypes
+
+console.log("2" > 1);
+console.log("02" > 1);
+
+console.log(null >= 0)
+console.log(null < 0)
+console.log(null == 0)
+
+// in JS comparison and equality works differently
+
+//=== it checks value and also datatypes

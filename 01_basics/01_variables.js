2d7e23ee71e3e783687c021ff7f4fbf23e9dcbf0
Sumit Mane <153489815+sumit4261@users.noreply.github.com>
2025-01-18 06:45:12 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const addressId = 1445533;
+let accountEmail = "something@gmail.com";
+var accountPassword = "9029202";
+
+/*
+prefer not to use var
+because of issue in block scope and functiional scope
+*/
